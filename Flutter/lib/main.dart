240248920d066f0070d7b061ec2aef1b723759ac
eslammdahy <eslammdahy@gmail.com>
2026-02-07 00:37:30 +0200

    Revert "auth + dashboard"
    This reverts commit b4235119ba357c2f8d5c31f25f7842afdc3417a1.

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -1,27 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:learnova/features/admin/dashboard/admin_dashboard_page.dart';
-import 'package:learnova/features/auth/forget_password/forget_password_page.dart';
-import 'package:learnova/features/auth/new%20password/set_new_password_page.dart';
-import 'package:learnova/features/auth/signup/signup_page.dart';
-import 'core/theme/app_theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'app.dart';
 
 void main() {
-  runApp(const LearnovaApp());
-}
-
-class LearnovaApp extends StatelessWidget {
-  const LearnovaApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-
-      home: AdminDashboardPage(),
-    );
-  }
+  runApp(const ProviderScope(child: App()));
 }
